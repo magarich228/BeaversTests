69c67216b43b9dc3b57cc927df17e03de9a4520e
magarich228 <kirillgroshev52@gmail.com>
2024-04-19 23:56:23 +0300

    Create new project command work.

---
+++ src/Services/TestsManager/BeaversTests.TestsManager.App/Commands/CreateProjectCommand.cs
@@ -0,0 +1,49 @@
+﻿using BeaversTests.Common.CQRS.Commands;
+using FluentValidation;
+
+namespace BeaversTests.TestsManager.App.Commands;
+
+public class CreateProjectCommand
+{
+    public class Command : ICommand<Result>
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class Result
+    {
+        public Guid ProjectId { get; set; }
+    }
+
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(1)
+                .MaximumLength(50);
+
+            RuleFor(c => c.Description)
+                .MaximumLength(1000);
+        }
+    }
+
+    public class Handler : ICommandHandler<Command, Result>
+    {
+        private readonly ITestsManagerContext _db;
+
+        public Handler(ITestsManagerContext db)
+        {
+            _db = db;
+        }
+        
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            // TODO:
+            throw new NotImplementedException("Implement create new project command logic.");
+        }
+    }
+}
---
+++ src/Services/TestsManager/BeaversTests.TestsManager.App/ITestsManagerContext.cs
@@ -0,0 +1,10 @@
+﻿using BeaversTests.TestsManager.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeaversTests.TestsManager.App;
+
+public interface ITestsManagerContext
+{ 
+    DbSet<TestProject> TestProjects { get; }
+    Task<int> SaveChangesAsync(CancellationToken ct);
+}
