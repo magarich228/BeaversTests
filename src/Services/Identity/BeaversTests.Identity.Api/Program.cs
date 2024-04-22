a635f02f6a92720760532ba857e083922bd293aa
magarich228 <kirillgroshev52@gmail.com>
2024-04-23 00:59:14 +0300

    Identity service layers added. AspNetIdentity package added.

--- src/Services/Identity/BeaversTests.Identity.Api/Program.cs
+++ src/Services/Identity/BeaversTests.Identity.Api/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
+
+services.AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(opt =>
+{
+    opt.Password.RequiredUniqueChars = 0;
+    opt.Password.RequireDigit = false;
+}).AddEntityFrameworkStores<IdentityDbContext<IdentityUser>>();
 
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
---
+++ src/Services/Identity/BeaversTests.Identity.Core/Models/BeaversUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BeaversTests.Identity.Core.Models;
+
+public class BeaversUser : IdentityUser<Guid>
+{
+    
+}
