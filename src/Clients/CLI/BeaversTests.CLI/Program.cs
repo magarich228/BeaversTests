a04bd4bb0d463cedfaf1f910bf66a746c63e230d
magarich228 <kirillgroshev52@gmail.com>
2024-07-28 22:39:10 +0300

    minio configuration fix. minio cli test.

--- src/Clients/CLI/BeaversTests.CLI/Program.cs
+++ src/Clients/CLI/BeaversTests.CLI/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine();
+﻿using Minio;
+using Minio.DataModel.Args;
+
+var client = new MinioClient()
+    .WithCredentials("kirill", "minio123")
+    .WithSSL(false)
+    .WithEndpoint("localhost:9000")
+    .Build();
+
+await client.MakeBucketAsync(new MakeBucketArgs().WithBucket("test"));
+
+var list = await client.ListBucketsAsync();
+
+Console.WriteLine(list.Buckets.Count);
