11131ffd715ef586685b0af26e8f55d3cb3f30df
magarich228 <kirillgroshev52@gmail.com>
2024-08-13 23:53:27 +0300

    Client work.

--- src/BuildingBlocks/BeaversTests.Client/TestsManagerClient.cs
+++ src/BuildingBlocks/BeaversTests.Client/TestsManagerClient.cs
@@ -1,16 +1,47 @@
-﻿namespace BeaversTests.Client;
+﻿using System.Net.Http.Json;
+
+namespace BeaversTests.Client;
 
 // TODO: move to abstractions
 public interface ITestsManagerClient
 {
-    Task AddTestPackageAsync(
-        Guid testPackageId,
-        IEnumerable<byte[]> testAssemblies,
-        IEnumerable<string> assemblyPaths,
+    Task<TestPackageIdResponse?> AddTestPackageAsync(
+        NewTestPackageDto newTestPackageDto,
         CancellationToken cancellationToken = default);
 }
 
-// public class TestsManagerClient : ServiceClientBase, ITestsManagerClient
-// {
-//     
-// }
+public class TestsManagerClient(Configuration configuration)
+    : BeaversServiceClientBase(configuration), 
+        ITestsManagerClient
+{
+    public async Task<TestPackageIdResponse?> AddTestPackageAsync(
+        NewTestPackageDto newTestPackageDto,
+        CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(newTestPackageDto);
+        
+        // TODO: get url from OpenApi?
+        var uri = new Uri("api/Tests/AddTestPackage");
+        var response = await HttpClient.PostAsJsonAsync(uri, newTestPackageDto, cancellationToken);
+        
+        response.EnsureSuccessStatusCode();
+        
+        return await response.Content.ReadFromJsonAsync<TestPackageIdResponse>(cancellationToken);
+    }
+
+}
+
+public class NewTestPackageDto
+{
+    public required string Name { get; init; }
+    public string? Description { get; init; }
+    public required IEnumerable<byte[]> TestAssemblies { get; init; }
+    public required IEnumerable<string> ItemPaths { get; init; }
+    public string? TestPackageType { get; init; }
+    public required Guid TestProjectId { get; init; }
+}
+
+public class TestPackageIdResponse
+{
+    public Guid TestPackageId { get; set; }
+}
