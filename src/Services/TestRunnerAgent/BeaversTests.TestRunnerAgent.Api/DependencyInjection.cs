f4115ec4282bee0289298212b7e2641bc5d71028
magarich228 <kirillgroshev52@gmail.com>
2024-11-08 15:27:41 +0300

    testRunnerAgent layers di added.

---
+++ src/Services/TestRunnerAgent/BeaversTests.TestRunnerAgent.Api/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using BeaversTests.Api.Shared.Middlewares;
+using BeaversTests.Common.CQRS.Abstractions;
+using BeaversTests.TestRunnerAgent.Events;
+using BeaversTests.TestRunnerAgent.Infrastructure.S3Access.Minio;
+
+namespace BeaversTests.TestRunnerAgent.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApi(this IServiceCollection services)
+    {
+        services.AddMediatR(conf => conf.RegisterServicesFromAssemblies(
+            typeof(BeaversTests.TestRunnerAgent.App.DependencyInjection).Assembly,
+            typeof(TestsStorageService).Assembly));
+        
+        return services;
+    }
+    
+    public static IApplicationBuilder UseApi(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ValidationErrorMiddleware>();
+
+        var messageBroker = app.ApplicationServices.GetRequiredService<IMessageBroker>();
+
+        // messageBroker.SubscribeAsync<TestRunnerFinalizedEvent>().Wait();
+        
+        return app;
+    }
+}
--- src/Services/TestRunnerAgent/BeaversTests.TestRunnerAgent.Api/Program.cs
+++ src/Services/TestRunnerAgent/BeaversTests.TestRunnerAgent.Api/Program.cs
@@ -1,8 +1,22 @@
+using BeaversTests.TestRunnerAgent.Api;
+using BeaversTests.TestRunnerAgent.App;
+using BeaversTests.TestRunnerAgent.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+services.AddTestRunnerAgentInfrastructure(configuration);
+services.AddTestRunnerControllerApp(configuration);
+services.AddApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Alive.");
+app.UseApi();
 
 app.Run();
 
 // TODO: подумать над окружениями для тестов в агенте
+// TODO: разгрести референсы для сервисов, удалить лишние Nuget пакеты
