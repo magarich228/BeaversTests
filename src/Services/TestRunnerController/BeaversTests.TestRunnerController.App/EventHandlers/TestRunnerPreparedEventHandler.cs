c6bbe82ec79e7fa44d51d3e7c91d6a3002168cb7
magarich228 <kirillgroshev52@gmail.com>
2024-11-10 01:00:54 +0300

    testRunnerAgent prepared event handler log.

--- src/Services/TestRunnerController/BeaversTests.TestRunnerController.App/EventHandlers/TestRunnerPreparedEventHandler.cs
+++ src/Services/TestRunnerController/BeaversTests.TestRunnerController.App/EventHandlers/TestRunnerPreparedEventHandler.cs
@@ -1,13 +1,19 @@
 ﻿using BeaversTests.Common.CQRS.Abstractions;
 using BeaversTests.Common.CQRS.Events;
 using BeaversTests.TestRunnerAgent.Events;
+using Microsoft.Extensions.Logging;
 
 namespace BeaversTests.TestRunnerController.App.EventHandlers;
 
-public class TestRunnerPreparedEventHandler(IEventBus eventBus) : IEventHandler<TestRunnerPreparedEvent>
+public class TestRunnerPreparedEventHandler(
+    IEventBus eventBus,
+    ILogger<TestRunnerPreparedEventHandler> logger) 
+    : IEventHandler<TestRunnerPreparedEvent>
 {
     public async Task Handle(TestRunnerPreparedEvent notification, CancellationToken cancellationToken)
     {
+        logger.LogInformation("TestRunnerPreparedEventHandler: handled {Notification}", notification.Id);
+        
         await eventBus.CommitLocalAsync(
             cancellationToken, 
             new BeaversTests.TestRunnerController.Events.TestRunnerPreparedEvent()
