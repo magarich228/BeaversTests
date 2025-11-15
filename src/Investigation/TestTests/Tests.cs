96591d75ad2c3635eab0e97300423e2f63baded5
magarich228 <kirillgroshev52@gmail.com>
2025-11-15 23:11:44 +0300

    nunit execution investigation.

---
+++ src/Investigation/TestTests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace TestTests;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void TestLog()
+    {
+        var logger = new Logger<Tests>(new LoggerFactory());
+        logger.LogInformation("test");
+        
+        Assert.Pass();
+    }
+}
---
+++ src/Investigation/TestTestsRunner/Program.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework.Api;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+var testAssemblyPath = args.Single();
+
+var runner = new NUnitTestAssemblyRunner(new DefaultTestAssemblyBuilder());
+
+var tests = runner.Load(testAssemblyPath, new Dictionary<string, object>());
+
+LogTest(tests);
+
+Console.WriteLine("\nExploring tests...\n");
+
+tests = runner.ExploreTests(TestFilter.Empty);
+
+LogTest(tests);
+
+void LogTest(ITest test, int level = 1)
+{
+    Console.WriteLine(new string(' ', level) + $"{test.Name} Cases: {test.TestCaseCount}");
+    
+    foreach (var child in test.Tests)
+    {
+        LogTest(child, level + 1);
+    }
+}
