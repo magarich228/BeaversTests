2220608feca2c8b4ac38236372574006b8d5f5d4
magarich228 <kirillgroshev52@gmail.com>
2025-11-17 23:24:56 +0300

    load nunit tests assembly dependencies investigation.

--- src/Investigation/TestTests/Tests.cs
+++ src/Investigation/TestTests/Tests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 
 namespace TestTests;
@@ -16,8 +18,20 @@ public class Tests
     public void TestLog()
     {
         var logger = new Logger<Tests>(new LoggerFactory());
-        logger.LogInformation("test");
+        logger.LogInformation("test test");
         
         Assert.Pass();
+    }
+
+    [Test]
+    public void DITest()
+    {
+        var services = new ServiceCollection();
+        
+        Assert.That(services.Count, Is.EqualTo(0));
+        
+        services.Add(new ServiceDescriptor(typeof(object), new object()));
+        
+        Assert.That(services.Count, Is.EqualTo(1));
     }
 }
--- src/Investigation/TestTestsRunner/Program.cs
+++ src/Investigation/TestTestsRunner/Program.cs
@@ -1,11 +1,25 @@
-﻿using NUnit.Framework.Api;
+﻿using System.Reflection;
+using NUnit.Framework.Api;
 using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
 
 var testAssemblyPath = args.Single();
 
+Console.WriteLine($"\nLoading test assembly: {testAssemblyPath}\n");
+
+Console.WriteLine($"Context assemblies: {AppDomain.CurrentDomain.GetAssemblies().Length} " + 
+                  string.Join(", ", AppDomain.CurrentDomain.GetAssemblies()
+                      .Select(x => x.GetName().Name)));
+
+var asm = Assembly.LoadFrom(testAssemblyPath);
+
+Console.WriteLine($"Context assemblies: {AppDomain.CurrentDomain.GetAssemblies().Length} " + 
+                  string.Join(", ", AppDomain.CurrentDomain.GetAssemblies()
+                      .Select(x => x.GetName().Name)));
+
 var runner = new NUnitTestAssemblyRunner(new DefaultTestAssemblyBuilder());
 
+// var tests = runner.Load(asm, new Dictionary<string, object>());
 var tests = runner.Load(testAssemblyPath, new Dictionary<string, object>());
 
 LogTest(tests);
@@ -16,6 +30,12 @@ tests = runner.ExploreTests(TestFilter.Empty);
 
 LogTest(tests);
 
+var results = runner.Run(TestListener.NULL, TestFilter.Empty);
+
+Console.WriteLine($"\nSuccess: {results.PassCount} Failed: {results.FailCount} Total: {results.TotalCount}");
+
+LogResult(results);
+
 void LogTest(ITest test, int level = 1)
 {
     Console.WriteLine(new string(' ', level) + $"{test.Name} Cases: {test.TestCaseCount}");
@@ -23,5 +43,15 @@ void LogTest(ITest test, int level = 1)
     foreach (var child in test.Tests)
     {
         LogTest(child, level + 1);
+    }
+}
+
+void LogResult(ITestResult testResult, int level = 1)
+{
+    Console.WriteLine(new string(' ', level) + $"{testResult.Name}: {testResult.ResultState.Label} {testResult.Message}");
+
+    foreach (var child in testResult.Children)
+    {
+        LogResult(child, level + 1);
     }
 }
